7ebe9be81aff3f89d19658dae5aaa9a994f52b55
Mart Haamer <marthaamer123@gmail.com>
2024-12-12 11:45:08 +0200

    Created project basic files

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import express, {Request, Response, NextFunction} from "express"
+
+const app = express()
+
+import todoRoutes from './routes/todos'
+
+app.use('/todos', todoRoutes)
+
+app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
+    res.json({ message: error.message });
+});
+
+app.listen(3011, () => {
+    console.log('Server is started at http://localhost:3011')
+})
